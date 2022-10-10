44c8790c74db2ece98b11e2c3f1af8ea647499fd
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 13:07:13 -0700

    test

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-
-# ******************************
-# Make your Code
-# ******************************
+numbers = [0, 0, 0, 0, 0]
+sum = 0
+for i in range(5):
+    numbers[i] = (int(input("Enter your input")))
+lowest = numbers[0]
+highest = numbers[0]
+for i in range(5):
+    if lowest > numbers[i]:
+        lowest = numbers[i]
+    if highest < numbers[i]:
+        highest = numbers[i]
+numbers.remove(lowest)
+numbers.remove(highest)
+for i in range(len(numbers)):
+    sum += numbers[i]
+print(sum)
